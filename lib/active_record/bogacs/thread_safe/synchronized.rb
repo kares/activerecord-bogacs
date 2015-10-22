522c7754d24c0a6a1d39bf4a5fc139ee126186f9
kares <self@kares.org>
2015-10-22 10:10:18 +0200

    re-invent (part of) thread_safe's CheapLockable as Synchronized

---
+++ lib/active_record/bogacs/thread_safe/synchronized.rb
@@ -0,0 +1,35 @@
+# @note Inpspired by thread_safe gem's Util::CheapLockable mixin.
+module ActiveRecord::Bogacs
+  module ThreadSafe
+    engine = defined?(RUBY_ENGINE) && RUBY_ENGINE
+    if engine == 'rbx'
+      module Synchronized
+        # Making use of the Rubinius' ability to lock via object headers to avoid
+        # the overhead of the extra Mutex objects.
+        def synchronize
+          ::Rubinius.lock(self)
+          begin
+            yield
+          ensure
+            ::Rubinius.unlock(self)
+          end
+        end
+        private :synchronize
+      end
+    elsif engine == 'jruby'
+      module Synchronized
+        require 'jruby'
+        # Use Java's native synchronized (this) { wait(); notifyAll(); } to avoid
+        # the overhead of the extra Mutex objects
+        def synchronize
+          ::JRuby.reference0(self).synchronized { yield }
+        end
+        private :synchronize
+      end
+    else
+      require 'thread'; require 'monitor'
+      # on MRI fallback to built-in MonitorMixin
+      Synchronized = MonitorMixin
+    end
+  end
+end
