74f3544ae4fecbbb73794aa16e003918304499e4
kares <self@kares.org>
2016-06-13 15:32:50 +0200

    autoload Reaper and Validator constants

--- lib/active_record/bogacs.rb
+++ lib/active_record/bogacs.rb
@@ -9,6 +9,8 @@ module ActiveRecord
     autoload :DefaultPool, 'active_record/bogacs/default_pool'
     autoload :FalsePool, 'active_record/bogacs/false_pool'
     autoload :ShareablePool, 'active_record/bogacs/shareable_pool'
+    autoload :Reaper, 'active_record/bogacs/reaper'
+    autoload :Validator, 'active_record/bogacs/validator'
   end
   autoload :SharedConnection, 'active_record/shared_connection'
 end
