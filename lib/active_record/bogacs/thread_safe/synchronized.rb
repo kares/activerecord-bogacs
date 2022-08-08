8d298b0ba26296cd7e0e5724f6743a17bcf0ad9f
kares <self@kares.org>
2022-08-08 16:19:52 +0200

    [refactor] simplify the Synchronized helper module

--- lib/active_record/bogacs/thread_safe/synchronized.rb
+++ lib/active_record/bogacs/thread_safe/synchronized.rb
@@ -1,21 +1,7 @@
 # @note Inpspired by thread_safe gem's Util::CheapLockable mixin.
 module ActiveRecord::Bogacs
   module ThreadSafe
-    engine = defined?(RUBY_ENGINE) && RUBY_ENGINE
-    if engine == 'rbx'
-      module Synchronized
-        # Making use of the Rubinius' ability to lock via object headers to avoid
-        # the overhead of the extra Mutex objects.
-        def synchronize
-          ::Rubinius.lock(self)
-          begin
-            yield
-          ensure
-            ::Rubinius.unlock(self)
-          end
-        end
-      end
-    elsif engine == 'jruby'
+    if defined? JRUBY_VERSION
       module Synchronized
         require 'jruby'
         # Use Java's native synchronized (this) { wait(); notifyAll(); } to avoid
