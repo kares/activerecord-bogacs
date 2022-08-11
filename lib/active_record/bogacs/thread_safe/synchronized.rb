16a66fea0b73e2615c3084642c30b53f600da33c
kares <self@kares.org>
2022-08-11 09:35:47 +0200

    [refactor] simplify the Synchronized mixin

--- lib/active_record/bogacs/thread_safe/synchronized.rb
+++ lib/active_record/bogacs/thread_safe/synchronized.rb
@@ -1,10 +1,12 @@
+require 'concurrent/thread_safe/util/cheap_lockable.rb'
+
 # @note Inpspired by thread_safe gem's Util::CheapLockable mixin.
 module ActiveRecord::Bogacs
   module ThreadSafe
     if defined? JRUBY_VERSION
-      module Synchronized
-        require 'jruby'
+      require 'jruby'
 
+      module Synchronized
         if defined? ::JRuby::Util.synchronized # since JRuby 9.3
           def synchronize
             ::JRuby::Util.synchronized(self) { yield }
@@ -16,9 +18,13 @@ module ActiveRecord::Bogacs
         end
       end
     else
-      require 'thread'; require 'monitor'
-      # on MRI fallback to built-in MonitorMixin
-      Synchronized = MonitorMixin
+      require 'concurrent/thread_safe/util/cheap_lockable.rb'
+
+      module Synchronized
+        include ::Concurrent::ThreadSafe::Util::CheapLockable
+        alias_method :synchronize, :cheap_synchronize
+        public :synchronize
+      end
     end
   end
 end
